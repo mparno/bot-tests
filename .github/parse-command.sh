697b5ec1ddb96e29bfd8de68583916824ddf36c9
Matthew Parno <mparno@soleaenergy.com>
2024-07-06 07:09:39 -0400

    Fixed politeness check.

--- .github/parse-command.sh
+++ .github/parse-command.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ $(echo $BODY | awk '{print $1}')=="@aeolus" ]]; then
-    if [[ $(echo $BODY | awk '{print $2}')=='please' ]]; then
+    if [[ "$(echo $BODY | awk '{print $2}')" == "please" ]]; then
         command=$(echo $BODY | awk '{for (i=3; i<NF; i++) printf $i " "; print $NF}')
         echo "Thank you for being polite.  I can certainly \"$command\"" 
     else
